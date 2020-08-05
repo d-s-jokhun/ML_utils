333b9d99fc719a6d19e148ff4f04e23bba01b6f1
D. S. Jokhun <d.s.jokhun@gmail.com>
2020-08-05 17:59:42 +0800

    added confusion mat

--- get_CompileParams.py
+++ get_CompileParams.py
@@ -11,12 +11,22 @@
 
 
 def get_CompileParams (model):
-    optimizer = getattr(model, 'optimizer', None)
-    loss = getattr(model, 'loss', None)
-    metrics = getattr(model, 'metrics', None)
-    loss_weights = getattr(model, 'loss_weights', None)
-    weighted_metrics = getattr(model, 'weighted_metrics', None)
-    run_eagerly = getattr(model, 'run_eagerly', None)
+    
+    try:
+        args = model._get_compile_args()
+        optimizer = args['optimizer']
+        loss = args['loss']
+        metrics = args['metrics']
+        loss_weights = args['loss_weights']
+        weighted_metrics = args['weighted_metrics']     
+        run_eagerly = getattr(model, 'run_eagerly', None)
+    except:    
+        optimizer = getattr(model, 'optimizer', None)
+        loss = getattr(model, 'loss', None)
+        metrics = getattr(model, 'metrics', None)
+        loss_weights = getattr(model, 'loss_weights', None)
+        weighted_metrics = getattr(model, 'weighted_metrics', None)
+        run_eagerly = getattr(model, 'run_eagerly', None)
         
     CompileParams={'optimizer':optimizer, 'loss':loss, 'metrics':metrics, 'loss_weights':loss_weights, 'weighted_metrics':weighted_metrics, 'run_eagerly':run_eagerly}
     
